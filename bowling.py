4b948867f8193d72f71873c9bc5b187e5f2cd916
Horvath Mate <ifjmate@gmail.com>
2017-11-30 15:29:36 +0100

    intruduced new function called, get_rollresult, cleaned up magic numbers, renamed game to roll, new dictionary in get_values

--- bowling.py
+++ bowling.py
@@ -1,45 +1,41 @@
-def score(game):
+def score(roll):
+    roll = [item.upper() for item in roll]
     result = 0
     frame = 1
+    strike = 'X'
+    spare = '/'
+    max_frame = 10
     in_first_half = True
-    for i in range(len(game)):
-        if game[i] == '/':
-            result += 10 - last
-        else:
-            result += get_value(game[i])
-        # if not in_first_half:
-            # frame += 1
-        if frame < 10  and get_value(game[i]) == 10:
-            if game[i] == '/':
-                result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
-                else:
-                    result += get_value(game[i+2])
-        last = get_value(game[i])
-        if not in_first_half:
-            frame += 1
-        if in_first_half == True:
+    for i in range(len(roll)):
+        result += get_rollresult(i, spare, i, roll)
+        if frame < max_frame:
+            if roll[i] == spare:
+                result += get_value(roll[i+1])
+            if roll[i] == strike:
+                result += get_value(roll[i+1])
+                result += get_rollresult(i+2, spare, i, roll)
+        if in_first_half:
             in_first_half = False
         else:
+            frame += 1
             in_first_half = True
-        if game[i] == 'X' or game[i] == 'x':
+        if roll[i] == strike:
             in_first_half = True
             frame += 1
     return result
 
+
 def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
-        return int(char)
-    elif char == 'X' or char == 'x':
-        return 10
-    elif char == '/':
-        return 10
-    elif char == '-':
-        return 0
+    bowling_values = {'X': 10, '/': 10, '-': 0}
+    bowling_values.update({str(i): i for i in range(1, 10)})
+    if char in bowling_values:
+        return bowling_values[char]
     else:
         raise ValueError()
+
+
+def get_rollresult(NumberOfRoll, ValueToCheck, NumberOfCurrentRoll, ResultList):
+    if ResultList[NumberOfRoll] == ValueToCheck:
+            return get_value(ResultList[NumberOfCurrentRoll]) - get_value(ResultList[NumberOfRoll-1])
+    else:
+        return get_value(ResultList[NumberOfRoll])
