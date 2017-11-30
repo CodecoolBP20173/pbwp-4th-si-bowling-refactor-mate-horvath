691b94d065c749a95ca021ef37f859b33fb8d603
Horvath Mate <ifjmate@gmail.com>
2017-11-30 17:11:06 +0100

    Added docstrings and 3 constants

--- bowling.py
+++ bowling.py
@@ -1,19 +1,26 @@
+STRIKE = 'X'
+SPARE = '/'
+MISS = '-'
+
+
 def score(throws):
+    """This function calculates the score of the bowling game from the given results(throws)
+    and return the calculated score(result)"""
+
     throws = throws.upper()
     result = 0
     frame = 1
-    strike = 'X'
-    spare = '/'
     max_frame = 10
     in_first_half = True
+
     for i in range(len(throws)):
-        result += get_throwresult(i, spare, i, throws)
+        result += get_throwresult(i, SPARE, i, throws)
         if frame < max_frame:
-            if throws[i] == spare:
+            if throws[i] == SPARE:
                 result += get_value(throws[i+1])
-            if throws[i] == strike:
+            if throws[i] == STRIKE:
                 result += get_value(throws[i+1])
-                result += get_throwresult(i+2, spare, i, throws)
+                result += get_throwresult(i+2, SPARE, i, throws)
                 frame += 1
                 continue
         if not in_first_half:
@@ -23,16 +30,24 @@ def score(throws):
 
 
 def get_value(char):
-    bowling_values = {'X': 10, '/': 10, '-': 0}
+    """This function assignes and then returns the corresponding value of the given bowling sign(char)"""
+
+    bowling_values = {STRIKE: 10, SPARE: 10, MISS: 0}
     bowling_values.update({str(i): i for i in range(1, 10)})
+
     if char in bowling_values:
         return bowling_values[char]
     else:
         raise ValueError()
 
 
-def get_throwresult(NumberOfthrows, ValueToCheck, NumberOfCurrentthrows, ResultList):
-    if ResultList[NumberOfthrows] == ValueToCheck:
-            return get_value(ResultList[NumberOfCurrentthrows]) - get_value(ResultList[NumberOfthrows-1])
+def get_throwresult(NumberOfthrow, ValueToCheck, NumberOfCurrentthrow, ResultList):
+    """This function calculates the result of the given roll, considering some all the resctrictions
+    that come with bowling point calculation
+    NumberOfthrow = the throw needed to check, ValueToCheck = the type of throw we need
+    NumberOfCurrentthrow = the current number of throw, ResultList = the object that holds the throws"""
+
+    if ResultList[NumberOfthrow] == ValueToCheck:
+            return get_value(ResultList[NumberOfCurrentthrow]) - get_value(ResultList[NumberOfthrow-1])
     else:
-        return get_value(ResultList[NumberOfthrows])
+        return get_value(ResultList[NumberOfthrow])
